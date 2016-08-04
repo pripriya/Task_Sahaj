3a25110e97b3c7dba7a7633a8f2a43a794d2929a
pripriya <priya13thjan@gmail.com>
2016-08-04 09:20:46 +0530

    Add files via upload

--- Desktop/Task/app/src/main/java/com/geval6/task/Core/MainActivity.java
+++ Desktop/Task/app/src/main/java/com/geval6/task/Core/MainActivity.java
@@ -32,20 +32,30 @@ public class MainActivity extends AppCompatActivity {
     private GridViewAdapter gridViewAdapter;
     private ArrayList<ModelClass> mGridData;
     private LinearLayout progressCircle;
-    private String url="https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=c7c7e36e3b626c1ff8c29eb9dfadc60b&user_id=144366085%40N08&format=json&nojsoncallback=1&api_sig=90f7e736b9f41cc20d95e63ebdf39d18";
+    private String URL="https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=c7c7e36e3b626c1ff8c29eb9dfadc60b&user_id=144366085%40N08&format=json&nojsoncallback=1&api_sig=90f7e736b9f41cc20d95e63ebdf39d18";
+    private String url="https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=80d5afc09c82a6777c60ac3bc8005254&user_id=144366085%40N08&format=json&nojsoncallback=1&api_sig=4fdc64854328b6e983532ab7c69a3733";
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         gridView = (GridView) findViewById(R.id.gridView);
         progressCircle = (LinearLayout) findViewById(R.id.progressCircle);
+        CheckInterenetConnectivity checkInterenetConnectivity = new CheckInterenetConnectivity();
 
         mGridData = new ArrayList<>();
         gridViewAdapter = new GridViewAdapter(this, R.layout.grid_item, mGridData);
         gridView.setAdapter(gridViewAdapter);
-        new AsyncHttpTask().execute(url);
 
+        if(checkInterenetConnectivity.getConnectivityStatus(this)==true){
+            new AsyncHttpTask().execute(url);
+        }
+
+        else if(checkInterenetConnectivity.getConnectivityStatus(this)==false){
+            Toast.makeText(this, "Please check your Internet Connection", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 
     public class AsyncHttpTask extends AsyncTask<String, Void, Integer> {
